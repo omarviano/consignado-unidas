2b91edec2a47f1a546e6e0a2bc5d48056fb32e02
Marcos Vinicius Alves <marcosvinicius@frwk.com.br>
2021-10-18 22:07:24 -0300

    feat: melhorias e ajustes

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,29 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { ExitToApp, ArrowBack, ArrowRightAlt } from '@material-ui/icons';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import { Checkbox, Grid, InputLabel } from '@material-ui/core';
-import { DatePicker } from '@material-ui/pickers';
+import { ExitToApp, ArrowBack } from '@material-ui/icons';
 
-import { Formik } from 'components/Formik';
-import { Input } from 'components/Inputs/Input';
-import { Button } from 'components/Buttons/Button';
+import { CPFForm } from './components/CPFForm';
+import { CompleteNameForm } from './components/CompleteNameForm';
+import { BirthDateForm } from './components/BirthDateForm';
+import { EmailForm } from './components/EmailForm';
+import { PhoneNumberForm } from './components/PhoneNumberForm';
+import { PasswordForm } from './components/PasswordForm';
+import { BankDataConfirmation } from './components/BankDataConfirmation';
+import { BankDataForm } from './components/BankDataForm';
 
 import * as Styled from './styles';
 
-const NUMBER_OF_STEPS = 9;
+const NUMBER_OF_STEPS = 8;
+
 const Registration: React.FC = () => {
   const [currentStep, setCurrentStep] = useState(0);
+  const [formsData, setFormsData] = useState<object>();
 
   const handleClickPrev = (): void => {
     if (currentStep > 0) setCurrentStep(state => state - 1);
   };
 
-  const handleClickNext = (): void => {
+  const nextStep = (): void => {
     if (currentStep + 1 < NUMBER_OF_STEPS) setCurrentStep(state => state + 1);
   };
+
+  const onSubmit = (data): void => {
+    setFormsData(state => ({ ...state, ...data }));
+
+    nextStep();
+  };
+
+  useEffect(() => {
+    console.log('formsData', formsData);
+  }, [formsData]);
 
   return (
     <>
@@ -34,349 +46,70 @@ const Registration: React.FC = () => {
         </Link>
       </Styled.Header>
 
-      <Formik
-        initialValues={{}}
-        onSubmit={values => console.log('values', values)}
-      >
-        <Styled.StepsContainer>
-          <Styled.Step step={0} currentStep={currentStep}>
-            <Styled.StepTitle>
-              Para começar a simulação, precisamos que informe alguns dados ok?
-              É bem rapidinho.
-            </Styled.StepTitle>
+      <Styled.StepsContainer>
+        <Styled.Step step={0} currentStep={currentStep}>
+          <CPFForm onSubmit={onSubmit} />
+        </Styled.Step>
 
-            <Input
-              name="cpf"
-              type="text"
-              label="Digite seu CPF"
-              placeholder="Digite seu CPF"
-              variant="outlined"
-              mask="999.999.999-99"
-            />
+        <Styled.Step step={1} currentStep={currentStep}>
+          <Styled.BackButton type="button" onClick={handleClickPrev}>
+            <ArrowBack />
+          </Styled.BackButton>
 
-            <Styled.ContinueButton
-              type="button"
-              size="small"
-              variant="contained"
-              color="primary"
-              onClick={handleClickNext}
-            >
-              Continuar <ArrowRightAlt />
-            </Styled.ContinueButton>
-          </Styled.Step>
+          <CompleteNameForm onSubmit={onSubmit} />
+        </Styled.Step>
 
-          <Styled.Step step={1} currentStep={currentStep}>
-            <Styled.BackButton type="button" onClick={handleClickPrev}>
-              <ArrowBack />
-            </Styled.BackButton>
+        <Styled.Step step={2} currentStep={currentStep}>
+          <Styled.BackButton type="button" onClick={handleClickPrev}>
+            <ArrowBack />
+          </Styled.BackButton>
 
-            <Styled.StepSmallTitle>
-              Qual o seu nome completo?
-            </Styled.StepSmallTitle>
+          {/* <BirthDateForm onSubmit={onSubmit} /> */}
+          <button type="button" onClick={nextStep}>
+            next
+          </button>
+        </Styled.Step>
 
-            <Input
-              name="name"
-              type="text"
-              label="Digite seu nome completo"
-              placeholder="Digite seu nome completo"
-              variant="outlined"
-            />
+        <Styled.Step step={3} currentStep={currentStep}>
+          <Styled.BackButton type="button" onClick={handleClickPrev}>
+            <ArrowBack />
+          </Styled.BackButton>
 
-            <Styled.ContinueButton
-              type="button"
-              size="small"
-              variant="contained"
-              color="primary"
-              onClick={handleClickNext}
-            >
-              Continuar <ArrowRightAlt />
-            </Styled.ContinueButton>
-          </Styled.Step>
+          <EmailForm onSubmit={onSubmit} />
+        </Styled.Step>
 
-          <Styled.Step step={2} currentStep={currentStep}>
-            <Styled.BackButton type="button" onClick={handleClickPrev}>
-              <ArrowBack />
-            </Styled.BackButton>
+        <Styled.Step step={4} currentStep={currentStep}>
+          <Styled.BackButton type="button" onClick={handleClickPrev}>
+            <ArrowBack />
+          </Styled.BackButton>
 
-            <Styled.StepSmallTitle>
-              Qual a sua data de nascimento?
-            </Styled.StepSmallTitle>
+          <PhoneNumberForm onSubmit={onSubmit} />
+        </Styled.Step>
 
-            <DatePicker
-              name="dtNasc"
-              value={new Date()}
-              onChange={newValue => {
-                console.log('newValue', newValue);
-              }}
-              label="Basic example"
-            />
+        <Styled.Step step={5} currentStep={currentStep}>
+          <Styled.BackButton type="button" onClick={handleClickPrev}>
+            <ArrowBack />
+          </Styled.BackButton>
 
-            <Styled.ContinueButton
-              type="button"
-              size="small"
-              variant="contained"
-              color="primary"
-              onClick={handleClickNext}
-            >
-              Continuar <ArrowRightAlt />
-            </Styled.ContinueButton>
-          </Styled.Step>
+          <PasswordForm onSubmit={onSubmit} />
+        </Styled.Step>
 
-          <Styled.Step step={3} currentStep={currentStep}>
-            <Styled.BackButton type="button" onClick={handleClickPrev}>
-              <ArrowBack />
-            </Styled.BackButton>
+        <Styled.Step step={6} currentStep={currentStep}>
+          <Styled.BackButton type="button" onClick={handleClickPrev}>
+            <ArrowBack />
+          </Styled.BackButton>
 
-            <Styled.StepSmallTitle>
-              Qual o seu melhor email?
-            </Styled.StepSmallTitle>
+          <BankDataConfirmation onSubmit={onSubmit} />
+        </Styled.Step>
 
-            <Input
-              name="email"
-              type="text"
-              label="Seu melhor email"
-              placeholder="Seu melhor email"
-              variant="outlined"
-            />
+        <Styled.Step step={7} currentStep={currentStep}>
+          <Styled.BackButton type="button" onClick={handleClickPrev}>
+            <ArrowBack />
+          </Styled.BackButton>
 
-            <Styled.ContinueButton
-              type="button"
-              size="small"
-              variant="contained"
-              color="primary"
-              onClick={handleClickNext}
-            >
-              Continuar <ArrowRightAlt />
-            </Styled.ContinueButton>
-          </Styled.Step>
-
-          <Styled.Step step={4} currentStep={currentStep}>
-            <Styled.BackButton type="button" onClick={handleClickPrev}>
-              <ArrowBack />
-            </Styled.BackButton>
-
-            <Styled.StepSmallTitle>Qual o seu celular?</Styled.StepSmallTitle>
-
-            <Input
-              name="cellphone"
-              type="text"
-              label="Digite o número do seu telefone"
-              placeholder="(XX)XXXXX-XXXX"
-              variant="outlined"
-              mask="(99)99999-9999"
-            />
-
-            <Styled.ContinueButton
-              type="button"
-              size="small"
-              variant="contained"
-              color="primary"
-              onClick={handleClickNext}
-            >
-              Continuar <ArrowRightAlt />
-            </Styled.ContinueButton>
-          </Styled.Step>
-
-          <Styled.Step step={5} currentStep={currentStep}>
-            <Styled.BackButton type="button" onClick={handleClickPrev}>
-              <ArrowBack />
-            </Styled.BackButton>
-
-            <Styled.StepSmallTitle>
-              Deseja fazer a simulação do seu empréstimo?
-            </Styled.StepSmallTitle>
-
-            <Input
-              name="password"
-              type="password"
-              label="Cadastre uma senha"
-              placeholder="Cadastre um senha"
-              variant="outlined"
-            />
-
-            <Input
-              name="passwordConfirmation"
-              type="password"
-              label="Confirme sua senha"
-              placeholder="Confirme sua senha"
-              variant="outlined"
-            />
-
-            <Styled.ContinueButton
-              type="button"
-              size="small"
-              variant="contained"
-              color="primary"
-              onClick={handleClickNext}
-            >
-              Continuar <ArrowRightAlt />
-            </Styled.ContinueButton>
-          </Styled.Step>
-
-          <Styled.Step step={6} currentStep={currentStep}>
-            <Styled.BackButton type="button" onClick={handleClickPrev}>
-              <ArrowBack />
-            </Styled.BackButton>
-
-            <Styled.StepSmallTitle>
-              Qual o seu vínculo trabalhista?
-            </Styled.StepSmallTitle>
-
-            <FormControl fullWidth>
-              <InputLabel id="demo-multiple-name-label">
-                Tipo de contrato
-              </InputLabel>
-
-              <Select
-                labelId="demo-simple-select-helper-label"
-                id="demo-simple-select-helper"
-                label="Age"
-                variant="outlined"
-                displayEmpty
-              >
-                <MenuItem value="CLT">CLT</MenuItem>
-                <MenuItem value="PJ">PJ</MenuItem>
-                <MenuItem value="E">Estágio</MenuItem>
-              </Select>
-            </FormControl>
-
-            <Styled.ContinueButton
-              type="button"
-              size="small"
-              variant="contained"
-              color="primary"
-              onClick={handleClickNext}
-            >
-              Continuar <ArrowRightAlt />
-            </Styled.ContinueButton>
-          </Styled.Step>
-
-          <Styled.Step step={7} currentStep={currentStep}>
-            <Styled.BackButton type="button" onClick={handleClickPrev}>
-              <ArrowBack />
-            </Styled.BackButton>
-
-            <Styled.BankDetailsConfirmationContainer>
-              <Styled.Hello>Olá João da Silva Ribeiro!</Styled.Hello>
-              <Styled.Email>joaodasilvaribeiro@gmail.com</Styled.Email>
-
-              <Styled.BankDetailsConfirmationTitle>
-                Você deseja informar os dados bancários para futuros
-                empréstimos?
-              </Styled.BankDetailsConfirmationTitle>
-              <Styled.BankDetailsConfirmationText>
-                Caso você não queira preeencher os dados, não se preocupe,
-                poderá cadsatrar em um outro momento ok? Lembrando que para esta
-                operação, só é possível utilizar conta corrente.
-              </Styled.BankDetailsConfirmationText>
-
-              <Styled.IAgreeTermsContainer>
-                <Checkbox />
-
-                <Styled.TermsText>
-                  Estou de acordo com os{' '}
-                  <Styled.TermsLink>termos e condições</Styled.TermsLink> da{' '}
-                  <Styled.TermsLink>política de privacidade</Styled.TermsLink>.
-                </Styled.TermsText>
-              </Styled.IAgreeTermsContainer>
-
-              <Styled.IAgreeTermsContainerButtons>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={handleClickNext}
-                >
-                  Sim
-                </Button>
-                <Button variant="outlined" color="primary">
-                  Agora não
-                </Button>
-              </Styled.IAgreeTermsContainerButtons>
-            </Styled.BankDetailsConfirmationContainer>
-          </Styled.Step>
-
-          <Styled.Step step={8} currentStep={currentStep}>
-            <Styled.BackButton type="button" onClick={handleClickPrev}>
-              <ArrowBack />
-            </Styled.BackButton>
-
-            <Styled.BankDataContainer>
-              <Styled.Hello>Olá João da Silva Ribeiro!</Styled.Hello>
-              <Styled.Email>joaodasilvaribeiro@gmail.com</Styled.Email>
-
-              <Styled.BankDetailsConfirmationTitle>
-                Você deseja informar os dados bancários para futuros
-                empréstimos?
-              </Styled.BankDetailsConfirmationTitle>
-              <Styled.BankDetailsConfirmationText>
-                Caso você não queira preeencher os dados, não se preocupe,
-                poderá cadsatrar em um outro momento ok? Lembrando que para esta
-                operação, <b>só é possível utilizar conta corrente</b>.
-              </Styled.BankDetailsConfirmationText>
-
-              <Styled.BankDataInputs>
-                <Input
-                  name="cc"
-                  type="text"
-                  label="Conta corrent"
-                  placeholder="Conta corrent"
-                  variant="outlined"
-                />
-
-                <Input
-                  name="cc"
-                  type="text"
-                  label="Banco"
-                  placeholder="Banco"
-                  variant="outlined"
-                />
-
-                <Grid container spacing={1}>
-                  <Grid item xs={6}>
-                    <Input
-                      name="agencia"
-                      type="text"
-                      label="Agência"
-                      placeholder="N° da sua agência"
-                      variant="outlined"
-                    />
-                  </Grid>
-
-                  <Grid item xs={7}>
-                    <Input
-                      name="numConta"
-                      type="text"
-                      label="N° da Conta Corrente"
-                      placeholder="XXXXXX"
-                      variant="outlined"
-                    />
-                  </Grid>
-
-                  <Grid item xs={2}>
-                    <Input
-                      name="digito"
-                      type="text"
-                      label="Digito"
-                      placeholder="Digito"
-                      variant="outlined"
-                    />
-                  </Grid>
-                </Grid>
-              </Styled.BankDataInputs>
-            </Styled.BankDataContainer>
-
-            <Styled.ContinueButton
-              type="submit"
-              size="small"
-              variant="contained"
-              color="primary"
-              onClick={handleClickNext}
-            >
-              Finalizar cadastro
-            </Styled.ContinueButton>
-          </Styled.Step>
-        </Styled.StepsContainer>
-      </Formik>
+          <BankDataForm onSubmit={onSubmit} />
+        </Styled.Step>
+      </Styled.StepsContainer>
     </>
   );
 };
--- src/pages/Registration/styles.ts
+++ src/pages/Registration/styles.ts
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { Button } from 'components/Buttons/Button';
 
 interface StepProps {
   step: number;
@@ -55,111 +54,9 @@ export const Step = styled.div<StepProps>`
   }
 `;
 
-export const StepTitle = styled.p`
-  max-width: 700px;
-  font-weight: 400;
-  font-size: 24px;
-  line-height: 26px;
-  text-align: center;
-  letter-spacing: 0.2px;
-  color: #646464;
-  margin-bottom: 24px;
-`;
-
-export const ContinueButton = styled(Button)`
-  margin-top: 64px;
-
-  svg {
-    margin-left: 16px;
-  }
-`;
-
 export const BackButton = styled.button`
   border: none;
   background: none;
   color: ${({ theme }) => theme.palette.grey[100]};
   margin: 0 auto 36px 0;
-`;
-
-export const StepSmallTitle = styled.p`
-  width: 100%;
-  font-weight: 400;
-  font-size: 18px;
-  line-height: 26px;
-  letter-spacing: 0.2px;
-  color: #646464;
-  margin-bottom: 50px;
-`;
-
-export const BankDetailsConfirmationContainer = styled.div``;
-
-export const Hello = styled.div`
-  font-size: 16px;
-  line-height: 26px;
-  letter-spacing: 0.2px;
-  color: #646464;
-`;
-
-export const Email = styled.div`
-  font-size: 14px;
-  line-height: 26px;
-  letter-spacing: 0.2px;
-  color: #848484;
-`;
-
-export const BankDetailsConfirmationTitle = styled.div`
-  font-size: 18px;
-  line-height: 26px;
-  letter-spacing: 0.2px;
-  color: #646464;
-  margin: 32px 0;
-  max-width: 450px;
-`;
-
-export const BankDetailsConfirmationText = styled.div`
-  font-size: 14px;
-  line-height: 26px;
-  letter-spacing: 0.2px;
-  color: #848484;
-  max-width: 450px;
-`;
-
-export const IAgreeTermsContainer = styled.div`
-  display: flex;
-  align-items: center;
-  max-width: 350px;
-  font-size: 14px;
-  line-height: 18px;
-  letter-spacing: 0.2px;
-  color: #848484;
-  margin-top: 32px;
-`;
-
-export const TermsText = styled.div``;
-
-export const TermsLink = styled.button`
-  border: 0;
-  background: none;
-  color: ${({ theme }) => theme.palette.primary.main};
-  text-decoration: underline;
-`;
-
-export const IAgreeTermsContainerButtons = styled.div`
-  display: flex;
-  margin-top: 32px;
-
-  button + button {
-    margin-left: 40px;
-  }
-`;
-
-export const BankDataContainer = styled.div``;
-
-export const BankDataInputs = styled.div`
-  margin-top: 32px;
-
-  .MuiGrid-container,
-  .MuiGrid-item + .MuiGrid-item {
-    margin-top: 30px;
-  }
 `;
