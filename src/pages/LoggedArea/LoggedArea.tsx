2e6c4b625ed10e11dd369e337f61b08ed27d2bf1
Francisco Junior <franciscojn49@gmail.com>
2021-10-27 10:48:50 -0300

    feat: conected with api simulite loan

--- src/pages/LoggedArea/LoggedArea.tsx
+++ src/pages/LoggedArea/LoggedArea.tsx
@@ -5,23 +5,42 @@ import { withContext } from 'utils/withContext';
 import { MarginUserProvider, useMarginUser } from './context';
 import { CardMarginAvailable } from './components/CardMarginAvailable';
 import { CardSimulateLoan } from './components/CardSimulateLoan';
+import { ModalSimulateLoan } from './components/ModalSimulateLoan';
+import {
+  ModalSimulateLoanProvider,
+  useModalSimulateLoan,
+} from './components/ModalSimulateLoan/context';
 
-const LoggedArea: FC = withContext(() => {
-  const { getMargin } = useMarginUser();
+const LoggedArea: FC = withContext(
+  () => {
+    const { getMargin, modalActive, statusCode } = useMarginUser();
+    const { toggleModal } = useModalSimulateLoan();
 
-  useEffect(() => {
-    getMargin();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    useEffect(() => {
+      getMargin();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-  return (
-    <RouteAccess typesOfAccess="auth">
-      <Layout>
-        <CardMarginAvailable />
-        <CardSimulateLoan />
-      </Layout>
-    </RouteAccess>
-  );
-}, MarginUserProvider);
+    useEffect(() => {
+      if (modalActive && statusCode !== 500) return toggleModal();
+      return () => {
+        <> </>;
+      };
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [modalActive]);
+
+    return (
+      <RouteAccess typesOfAccess="auth">
+        <Layout>
+          <CardMarginAvailable />
+          <CardSimulateLoan />
+          <ModalSimulateLoan />
+        </Layout>
+      </RouteAccess>
+    );
+  },
+  MarginUserProvider,
+  ModalSimulateLoanProvider,
+);
 
 export { LoggedArea };
