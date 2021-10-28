3dca28324a77194498a0257a6593e96d36e25e97
Francisco Junior <franciscojn49@gmail.com>
2021-10-28 14:54:29 -0300

    feat(creted component table): created component Table

--- src/pages/SimulateLoan/SimulateLoan.tsx
+++ src/pages/SimulateLoan/SimulateLoan.tsx
@@ -3,6 +3,7 @@ import { RouteAccess } from 'components/RouteAccess';
 import { FC } from 'react';
 
 import { CardSimulateLoan } from './components/CardSimulateLoan';
+import { Table } from './components/Table';
 
 const SimulateLoan: FC = () => (
   <RouteAccess typesOfAccess="auth">
@@ -13,6 +14,7 @@ const SimulateLoan: FC = () => (
       }}
     >
       <CardSimulateLoan />
+      <Table />
     </Layout>
   </RouteAccess>
 );
