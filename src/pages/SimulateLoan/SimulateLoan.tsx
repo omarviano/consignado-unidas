0c036223edc410002ae996b9f12e1c1a8149cfcb
Marcos Vinicius Alves <marcosvinicius@frwk.com.br>
2021-11-03 16:01:30 -0300

    feat: implementações

--- src/pages/SimulateLoan/SimulateLoan.tsx
+++ src/pages/SimulateLoan/SimulateLoan.tsx
@@ -2,21 +2,32 @@ import { FC, useEffect, useMemo, useState } from 'react';
 import { Layout } from 'components/Layout';
 import { RouteAccess } from 'components/RouteAccess';
 import { GridColumns, GridRowId } from '@mui/x-data-grid';
+import { Modal } from '@mui/material';
+import { Close, CheckCircle, Cancel } from '@mui/icons-material';
+import { useHistory } from 'react-router-dom';
 
+import { getToken } from 'hooks/auth/storage';
+import useModal from 'hooks/modal';
 import { SimulateLoanProvider, useSimulateLoan } from 'hooks/simulate';
 import { withContext } from 'utils/withContext';
 import { Table } from 'components/Table';
 import { useSimulateLoanRealTime } from 'hooks/simulateRealtime';
 import { formatValue } from 'utils/formatValue';
 import { TableSimulateProps } from 'interface/tableSimulate';
+import { Button } from 'components/Buttons/Button';
+import { RoutingPath } from 'utils/routing';
 import { CardSimulateLoan } from './components/CardSimulateLoan';
 
 import * as Styled from './styles';
 
 const SimulateLoan: FC = withContext(() => {
+  const history = useHistory();
   const { dataSimulateLoan } = useSimulateLoanRealTime();
   const { requestStatus } = useSimulateLoan();
   const [tableData, setTableData] = useState<TableSimulateProps[]>([]);
+  const { open: modalSuccesOpen, toggle: toggleModalSucces } = useModal();
+  const { open: modalErrorOpen, toggle: toggleModalError } = useModal();
+  const { open: modalConfirmOpen, toggle: toggleModalConfirm } = useModal();
 
   useEffect(() => {
     const data = dataSimulateLoan.installments.map(item => ({
@@ -68,6 +79,10 @@ const SimulateLoan: FC = withContext(() => {
     [],
   );
 
+  const goToAccompaniment = () => {
+    history.push(RoutingPath.ACCOMPANIMENT);
+  };
+
   return (
     <RouteAccess typesOfAccess="auth">
       <Layout
@@ -99,9 +114,82 @@ const SimulateLoan: FC = withContext(() => {
           rows={tableData}
         />
 
-        <Styled.RequestButton type="button">
-          Solicitar Empréstimo
-        </Styled.RequestButton>
+        <Styled.ContainerButton>
+          <Styled.RequestButton type="button" variant="contained">
+            Solicitar Empréstimo
+          </Styled.RequestButton>
+        </Styled.ContainerButton>
+
+        <Modal open={modalSuccesOpen} onClose={toggleModalSucces}>
+          <Styled.ModalSuccessContent>
+            <Styled.CloseModalButton type="button" onClick={toggleModalSucces}>
+              <Close fontSize="small" color="primary" />
+            </Styled.CloseModalButton>
+
+            <CheckCircle className="success-icon" />
+
+            <Styled.ModalText>Solicitação enviada!</Styled.ModalText>
+
+            <Styled.ModalText>
+              Sua solicitação será analisada. Assim que tivermos a resposta,
+              entraremos em contato por email e por aqui. Qualquer dúvida, entre
+              em contato com RH da empresa.
+            </Styled.ModalText>
+
+            <Button
+              type="button"
+              variant="contained"
+              className="redirect-button"
+              onClick={goToAccompaniment}
+            >
+              Acompanhar
+            </Button>
+          </Styled.ModalSuccessContent>
+        </Modal>
+
+        <Modal open={modalErrorOpen} onClose={toggleModalError}>
+          <Styled.ModalErrorContent>
+            <Styled.CloseModalButton type="button" onClick={toggleModalError}>
+              <Close fontSize="small" color="primary" />
+            </Styled.CloseModalButton>
+
+            <Cancel className="cancel-icon" />
+
+            <Styled.ModalText>Solicitação negada</Styled.ModalText>
+
+            <Styled.ModalText>
+              A solicitação do seu empréstimo infelizmente não poderá ser
+              realizada. Por favor, entre em contato com o RH.
+            </Styled.ModalText>
+          </Styled.ModalErrorContent>
+        </Modal>
+
+        <Modal open={modalConfirmOpen} onClose={toggleModalConfirm}>
+          <Styled.ModalConfirmContent>
+            <Styled.CloseModalButton type="button" onClick={toggleModalConfirm}>
+              <Close fontSize="small" color="primary" />
+            </Styled.CloseModalButton>
+
+            <Styled.ModalConfirmHello>
+              Olá, {getToken()?.user.name}! Tudo bem?
+              <b>Você confirma os seus dados abaixo?</b>
+            </Styled.ModalConfirmHello>
+
+            <Styled.ModalConfirmData>
+              email: {getToken()?.user.email}
+              <br />
+              telefone: {getToken()?.user.phoneNumber}
+            </Styled.ModalConfirmData>
+
+            <Button
+              type="button"
+              className="confirm-button"
+              variant="contained"
+            >
+              Confirmar
+            </Button>
+          </Styled.ModalConfirmContent>
+        </Modal>
       </Layout>
     </RouteAccess>
   );
