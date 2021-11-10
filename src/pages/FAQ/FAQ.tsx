7404ba6e1d5f47cc03cb95b9a654b7246c2cde93
Francisco Junior <franciscojn49@gmail.com>
2021-11-10 17:56:47 -0300

    feat: add accordion

--- src/pages/FAQ/FAQ.tsx
+++ src/pages/FAQ/FAQ.tsx
@@ -1,20 +1,124 @@
+import { FC, useState } from 'react';
 import { Layout } from 'components/Layout';
 import { RouteAccess } from 'components/RouteAccess';
-import { FC } from 'react';
 
-// import { Container } from './styles';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import { ExpandMore, Add, Remove } from '@mui/icons-material';
 
-const FAQ: FC = () => (
-  <RouteAccess typesOfAccess="auth">
-    <Layout
-      containerStyles={{
-        maxWidth: '1276px',
-        padding: 0,
-      }}
-    >
-      <h1>FAQ</h1>
-    </Layout>
-  </RouteAccess>
-);
+import * as Styled from './styles';
+
+const FAQ: FC = () => {
+  const [expanded, setExpanded] = useState<string | false>(false);
+
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false);
+    };
+  return (
+    <RouteAccess typesOfAccess="auth">
+      <Layout
+        containerStyles={{
+          maxWidth: '1276px',
+          padding: 0,
+        }}
+      >
+        <Styled.Container>
+          <div>
+            <Accordion
+              expanded={expanded === 'panel1'}
+              onChange={handleChange('panel1')}
+              style={{
+                marginBottom: '10px',
+                border: ' 1px solid #C4C4C4',
+                background: 'transparent',
+                width: '1000px',
+                marginLeft: '20px',
+                filter: 'drop-shadow(0px 2px 6px rgba(46, 43, 80, 0.25))',
+                boxShadow: 'none',
+                borderRadius: '4px',
+              }}
+            >
+              <AccordionSummary
+                expandIcon={
+                  expanded === 'panel1' ? <Remove color="secondary" /> : <Add />
+                }
+                aria-controls="panel4bh-content"
+                id="panel4bh-header"
+              >
+                <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                  Personal data
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Nunc vitae orci ultricies, auctor nunc in, volutpat nisl.
+                  Integer sit amet egestas eros, vitae egestas augue. Duis vel
+                  est augue.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+
+            <Accordion
+              expanded={expanded === 'panel2'}
+              onChange={handleChange('panel2')}
+              style={{
+                marginBottom: '10px',
+              }}
+            >
+              <AccordionSummary
+                expandIcon={
+                  expanded === 'panel2' ? <Remove color="secondary" /> : <Add />
+                }
+                aria-controls="panel4bh-content"
+                id="panel4bh-header"
+              >
+                <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                  Personal data
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Nunc vitae orci ultricies, auctor nunc in, volutpat nisl.
+                  Integer sit amet egestas eros, vitae egestas augue. Duis vel
+                  est augue.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+
+            <Accordion
+              expanded={expanded === 'panel3'}
+              onChange={handleChange('panel3')}
+              style={{
+                marginBottom: '10px',
+              }}
+            >
+              <AccordionSummary
+                expandIcon={
+                  expanded === 'panel3' ? <Remove color="secondary" /> : <Add />
+                }
+                aria-controls="panel4bh-content"
+                id="panel4bh-header"
+              >
+                <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                  Personal data
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Nunc vitae orci ultricies, auctor nunc in, volutpat nisl.
+                  Integer sit amet egestas eros, vitae egestas augue. Duis vel
+                  est augue.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+        </Styled.Container>
+      </Layout>
+    </RouteAccess>
+  );
+};
 
 export { FAQ };
--- src/pages/FAQ/styles.ts
+++ src/pages/FAQ/styles.ts
@@ -1,3 +1,6 @@
 import styled from 'styled-components';
+import { Card as MUICard } from '@mui/material';
 
-export const Container = styled.div``;
+export const Container = styled(MUICard)`
+  margin-top: 100px;
+`;
