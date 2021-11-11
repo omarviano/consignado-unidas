5001225c808e311c3a9cb43a14561a9e3211a8b4
Francisco Junior <franciscojn49@gmail.com>
2021-11-11 17:05:09 -0300

    feat(add label in input and select): add label in input and select

--- src/pages/Registration/components/CPFForm/styles.ts
+++ src/pages/Registration/components/CPFForm/styles.ts
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 import { Input } from 'components/Inputs/Input';
 
+export const ContentCPFInput = styled.div`
+  margin-top: 60px;
+`;
+
 export const CPFInput = styled(Input)`
   max-width: 450px;
-  margin-top: 60px;
 `;
