29011b858174bf9fd04a562897b1d8cb432c8591
Marcos Vinicius Alves <marcosvinicius@frwk.com.br>
2021-12-01 14:41:54 -0300

    feat: correção datepicker e mais  ajustes

--- src/pages/Registration/components/BirthDateForm/BirthDateForm.tsx
+++ src/pages/Registration/components/BirthDateForm/BirthDateForm.tsx
@@ -2,24 +2,20 @@ import React from 'react';
 
 import { Formik } from 'components/Formik';
 import { DatePicker } from 'components/DatePicker';
-import { TextField } from '@mui/material';
 import { StepSmallTitle } from '../StepSmallTitle';
 import { ContinueButton } from '../ContinueButton';
 
+import * as Styled from './styles';
 import { schema } from './schema';
-
 import { BirthDateFormProps } from './props';
 
 const BirthDateForm: React.FC<BirthDateFormProps> = ({ onSubmit }) => (
   <Formik initialValues={{}} validationSchema={schema} onSubmit={onSubmit}>
     <StepSmallTitle>Qual a sua data de nascimento?</StepSmallTitle>
 
-    <DatePicker
-      value=""
-      name="birthDate"
-      maxDate={new Date(Date.now())}
-      renderInput={params => <TextField {...params} />}
-    />
+    <Styled.InputContainer>
+      <DatePicker value="" name="birthDate" disableFuture />
+    </Styled.InputContainer>
 
     <ContinueButton
       type="submit"
