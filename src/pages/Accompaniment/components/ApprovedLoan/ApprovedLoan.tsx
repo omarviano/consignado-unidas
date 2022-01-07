16aa2e1a26264c2c8ee9b72546e8c6f2a5e6e5e8
Marcos Vinicius Alves <marcosvinicius@frwk.com.br>
2022-01-07 14:58:45 -0300

    feat: merge

--- src/pages/Accompaniment/components/ApprovedLoan/ApprovedLoan.tsx
+++ src/pages/Accompaniment/components/ApprovedLoan/ApprovedLoan.tsx
@@ -65,11 +65,7 @@ const ApprovedLoan: FC = () => {
           name: response?.name,
           nationality: response?.nationality,
           professional: response?.professional,
-<<<<<<< HEAD
-          number: response?.number,
-=======
           number: response?.number || '',
->>>>>>> a737dbfa97f042c76c0ae4b3147448c235446f2d
           complement: response?.complement,
           bankCode: String(response?.bankCode),
           agency: response?.agency,
