0cc95cd43a82a7782ef80565bc12235a2b1d0c37
Francisco Junior <franciscojn49@gmail.com>
2022-01-27 10:51:50 -0300

    feat: add sonar-report.xml

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,4 +1,4 @@
-export function formatDate(date: Date | null): string | null {
+export function formatDate(date: Date | null | undefined): string | null {
   if (!date) return null;
 
   return new Intl.DateTimeFormat('pt-BR', { timeZone: 'UTC' }).format(
