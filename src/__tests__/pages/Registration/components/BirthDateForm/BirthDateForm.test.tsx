43d52794d0947be0044863fb2bf3466e2e055d25
Marcos Vinicius Alves <marcosvinicius@frwk.com.br>
2022-01-31 15:00:29 -0300

    feat: testes dos steps finalizados

--- src/__tests__/pages/Registration/components/BirthDateForm/BirthDateForm.test.tsx
+++ src/__tests__/pages/Registration/components/BirthDateForm/BirthDateForm.test.tsx
@@ -38,20 +38,115 @@ describe('Component: <BirthDateForm />', () => {
   });
 
   test('should be able to show required field', async () => {
+    const onSubmit = jest.fn();
+
     const { container } = render(
       <Providers>
-        <BirthDateForm onSubmit={() => null} />
+        <BirthDateForm onSubmit={onSubmit} />
       </Providers>,
     );
 
-    const input = screen.getByRole('textbox');
-    fireEvent.focusOut(input);
+    const form = screen.getByTestId('form');
+    fireEvent.submit(form);
 
     await waitFor(() => {
       expect(
         container.querySelector('#dateInput_birthDate-helper-text')
           ?.textContent,
       ).toBe('Informe sua data de nascimento');
+
+      expect(onSubmit).not.toBeCalled();
+    });
+  });
+
+  test('should be able to not submit form', async () => {
+    const onSubmit = jest.fn();
+
+    render(
+      <Providers>
+        <BirthDateForm onSubmit={onSubmit} />
+      </Providers>,
+    );
+
+    const form = screen.getByTestId('form');
+    fireEvent.submit(form);
+
+    await waitFor(async () => {
+      expect(onSubmit).not.toBeCalled();
+    });
+  });
+
+  test('should be able to show future date not allowed ', async () => {
+    const onSubmit = jest.fn();
+
+    const { container } = render(
+      <Providers>
+        <BirthDateForm onSubmit={onSubmit} />
+      </Providers>,
+    );
+
+    const form = screen.getByTestId('form');
+
+    const input = screen.getByTestId('date-picker_birthDate');
+
+    fireEvent.change(input, { target: { value: `01/01/2100` } });
+
+    fireEvent.submit(form);
+
+    await waitFor(async () => {
+      expect(
+        container.querySelector('#dateInput_birthDate-helper-text')
+          ?.textContent,
+      ).toBe('Data de nascimento futura não é permitida');
+      expect(onSubmit).not.toBeCalled();
+    });
+  });
+
+  test('should be able to show invalid date', async () => {
+    const onSubmit = jest.fn();
+
+    const { container } = render(
+      <Providers>
+        <BirthDateForm onSubmit={onSubmit} />
+      </Providers>,
+    );
+
+    const form = screen.getByTestId('form');
+
+    const input = screen.getByTestId('date-picker_birthDate');
+
+    fireEvent.change(input, { target: { value: `01/00/210` } });
+
+    fireEvent.submit(form);
+
+    await waitFor(async () => {
+      expect(
+        container.querySelector('#dateInput_birthDate-helper-text')
+          ?.textContent,
+      ).toBeDefined();
+      expect(onSubmit).not.toBeCalled();
+    });
+  });
+
+  test('should be able to submit form', async () => {
+    const onSubmit = jest.fn();
+
+    render(
+      <Providers>
+        <BirthDateForm onSubmit={onSubmit} />
+      </Providers>,
+    );
+
+    const form = screen.getByTestId('form');
+
+    const input = screen.getByTestId('date-picker_birthDate');
+
+    fireEvent.change(input, { target: { value: `12/05/1997` } });
+
+    fireEvent.submit(form);
+
+    await waitFor(async () => {
+      expect(onSubmit).toBeCalled();
     });
   });
 });
