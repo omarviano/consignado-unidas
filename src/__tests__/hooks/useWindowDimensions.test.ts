e2fa4ba21b32c59b731c684ef54ed25fe97375ff
Francisco Junior <franciscojn49@gmail.com>
2022-02-09 14:52:18 -0300

    feat: add test hook dimensions

--- src/__tests__/hooks/useWindowDimensions.test.ts
+++ src/__tests__/hooks/useWindowDimensions.test.ts
@@ -1,11 +1,21 @@
-import { act, renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react-hooks';
 
 import useWindowDimensions from 'hooks/useWindowDimensions';
 
 describe('Hook window dimensions', () => {
-  test('should be able to exwcute window dimensions', () => {
-    const { width } = useWindowDimensions();
+  test('should be able to execute window dimensions width', () => {
+    const { result } = renderHook(() => useWindowDimensions());
+
+    const width = result.current.width && result.current.width > 720;
 
     expect(width).toBeTruthy();
+  });
+
+  test('should be able to execute window dimensions height', () => {
+    const { result } = renderHook(() => useWindowDimensions());
+
+    const height = result.current.height && result.current.height > 720;
+
+    expect(height).toBeTruthy();
   });
 });
